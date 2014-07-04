ad0772dc16a0c5fa0cdf222e06c5e4267e1242d1
briere.r@gmail.com <briere.r@gmail.com>
2014-07-04 13:48:50 -0400

    requirejs

---
+++ static/js/workspace/app.js
@@ -0,0 +1,39 @@
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'static/js/lib',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        workspace: '../workspace'
+    }
+});
+
+// Start the main app logic.
+requirejs([
+        'jquery-1.10.2',
+        'jquery-ui',
+        'bootstrap',
+        'codemirror',
+        'jquery.handsontable.full',
+        'vendor/d3.layout.min',
+        'vendor/d3.min',
+        'rickshaw',
+        'workspace/carafe-scheme',
+        'workspace/chart',
+        'workspace/evaluator',
+        'workspace/eqobj',
+        'workspace/handsontable-fabric',
+        'workspace/rickshaw-fabric',
+        'workspace/table',
+        'workspace/validator',
+        'workspace/widget',
+        'workspace/workspace',
+        'jquery.mockjax',
+        'workspace/mock',
+    ],
+    function() {
+
+    });
